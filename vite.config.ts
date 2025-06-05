56228fe7560cd62f1bbaafcdcc2ed692cd6332c1
Esteban Vega <iestebanvega1@gmail.com>
2025-06-05 15:05:59 -0500

    build: add node types and refactor vite config
    - Add @types/node and @types/path-type dependencies
    - Update tsconfig to include node types
    - Refactor vite config to separate vite and vitest configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,25 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
+import { defineConfig as defineVitestConfig, mergeConfig } from "vitest/config";
+import { defineConfig as defineViteConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
+const viteConfig = defineViteConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": "/src",
     },
   },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/app/setupTests.ts",
-    css: true, // If you want to test components with CSS imports
-  },
 });
+
+export default mergeConfig(
+  viteConfig,
+  defineVitestConfig({
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/app/setupTests.ts",
+      css: true,
+    },
+  })
+);
